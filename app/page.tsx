639fc17d48df51afa1d09ae69bca26b8c01bcb3f
Faraz Wasim Sualeh <24bit44@aiktc.ac.in>
2026-08-19 13:14:12 +0000

    Test protected workflow

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Paperwork
           </a>
         </div>
       </main>
